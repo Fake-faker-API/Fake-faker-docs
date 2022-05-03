ab3dfada0b5f79309a064dca0d7e41e5e4f95a79
Olga <41551585+olgashi@users.noreply.github.com>
2022-05-03 09:45:25 -0500

    Update file name, import statement (#8)

--- src/utils/parseResponseObject.tsx
+++ src/utils/parseResponseObject.tsx
@@ -2,7 +2,7 @@ import parse from 'html-react-parser';
 import { ResponseObj } from '../components/EndpointDescription';
 const JSON5 = require('json5');
 
-export function parseResponseObject (jsonObj: ResponseObj) {
+export default function parseResponseObject (jsonObj: ResponseObj) {
   return parse(JSON5.stringify(jsonObj, function (k:string, v:string|Number) {
     if (typeof v === 'string') return `<span class='response-example-string-value'>${v}</span>`
     else if (typeof v === 'number') return `<span class='response-example-number-value'>${v}</span>`
